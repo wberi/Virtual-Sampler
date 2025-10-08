7339ea650ba5dd3fcf9f4e47a9e43f8157d0a8b2
wberi <mberi5002@gmail.com>
2025-10-08 11:33:18 +0200

    Added basic UI + fixed CMakeLists.txt

--- source/include/UI/base.hpp
+++ source/include/UI/base.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+  #include <wx/wx.h>
+#endif
+
 ///A MainApp osztály
 ///
 ///Ez kezel minden eseményt, ami az alkalmazásban történik.
@@ -14,8 +19,26 @@ class MainApp: public wxApp
 ///Az vizuális elemek megjelenítéséért felelős osztály.
 class MainFrame: public wxFrame 
 {
-  public:
-    MainFrame(const wxString &title, const wxPoint &pos, const wxSize &size); 
+public:
+    MainFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+    //declare widget
+    wxMenuBar* mainMenu;  
+
+    //declare events
+    void saveProfile(wxCommandEvent& event);
+    void loadProfile(wxCommandEvent& event);
+    void onExit(wxCommandEvent& event);
+
+    DECLARE_EVENT_TABLE()  
+};
+
+//button IDs
+enum
+{
+  MENU_SAVE,
+  MENU_LOAD,
+  MENU_QUIT,
 };
 
 DECLARE_APP(MainApp);
--- source/src/UI/base.cpp
+++ source/src/UI/base.cpp
@@ -1,27 +1,65 @@
-#include <wx/wxprec.h>
+#include <base.hpp>
+#include <messages.hpp>
 
-#ifndef WX_PRECOMP
-  #include <wx/wx.h>
-#endif // !WX_PRECOMP
-
-#include <UI/base.hpp>
+const int WIDTH = 300;
+const int HEIGHT = 300;
 
 IMPLEMENT_APP(MainApp);
 
 bool MainApp::OnInit()
 {
-  MainFrame* main = new MainFrame(_("Virtual Sampler"), wxDefaultPosition, wxSize(300, 300));
+  MainFrame* main = new MainFrame(wxT("Virtual Sampler"), wxDefaultPosition, wxSize(WIDTH, HEIGHT));
   main->Show(true);
   SetTopWindow(main);
+
+  //Message window setup
+  MessageFrame *msgs = new MessageFrame(wxT("Messages"));
+  msgs->Show(true);
+
   return true;
 }
 
-MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size): 
-  wxFrame((wxFrame*) NULL, -1, title, pos, size)
+//event table init
+BEGIN_EVENT_TABLE(MainFrame, wxFrame)
+  EVT_MENU(MENU_SAVE, MainFrame::saveProfile)
+  EVT_MENU(MENU_LOAD, MainFrame::loadProfile)
+  EVT_MENU(MENU_QUIT, MainFrame::onExit)
+END_EVENT_TABLE()
+  
+
+MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size) 
+:wxFrame((wxFrame*) NULL, -1, title, pos, size)
 {
   //init buttons and other widgets here
+  mainMenu = new wxMenuBar();
+  wxMenu* profileMenu = new wxMenu();
+  wxMenu* recordMenu = new wxMenu();
+  wxMenu* aboutMenu = new wxMenu();
+
+  //add menu options to the "Profile" menu
+  profileMenu->Append(MENU_SAVE, wxT("Save profile\tCtrl-S"));
+  profileMenu->Append(MENU_LOAD, wxT("Load profile\tCtrl-O"));
+  profileMenu->Append(MENU_QUIT, wxT("Quit\tCtrl-Q"));
+
+  //add "Profile" to the bar and activate the bar
+  mainMenu->Append(profileMenu, wxT("Profile"));
+  mainMenu->Append(recordMenu, wxT("Recording"));
+  mainMenu->Append(aboutMenu, wxT("About"));
+  SetMenuBar(mainMenu);
 }
 
+//Events
+void MainFrame::saveProfile(wxCommandEvent& event)
+{
+ //TODO: implement
+}
 
+void MainFrame::loadProfile(wxCommandEvent& event)
+{
+  //TODO: implement
+}
 
-
+void MainFrame::onExit(wxCommandEvent& event)
+{
+  Close(true);
+}
