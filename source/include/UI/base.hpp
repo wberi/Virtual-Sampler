f8bf73acab2a4b00a0ccb4c8fa6c88aa55f5d401
wberi <mberi5002@gmail.com>
2025-10-10 12:39:48 +0200

    Started button grid

--- source/include/UI/base.hpp
+++ source/include/UI/base.hpp
@@ -10,13 +10,23 @@
 ///Az vizuális elemek megjelenítéséért felelős osztály.
 class MainFrame: public wxFrame 
 {
+private:
+  const double margin = 10;
 public:
     MainFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
 
     //declare widget
-    wxMenuBar* mainMenu;  
+    wxMenuBar* mainMenu;
+    wxPanel* panel;
+    wxBoxSizer* windowSizer;
+    wxFlexGridSizer* buttonGridSizer;
+    wxMenu* profileMenu;
+    wxMenu* recordMenu;
+    wxMenu* aboutMenu;
 
     //declare events
+    void pressPlayButton(wxCommandEvent& event);
+
     void saveProfile(wxCommandEvent& event);
     void loadProfile(wxCommandEvent& event);
     void showAbout(wxCommandEvent& event);
@@ -28,6 +38,7 @@ public:
 //button IDs
 enum
 {
+  BUTTON_PLAY,
   MENU_SAVE,
   MENU_LOAD,
   MENU_ABOUT,
--- source/src/UI/base.cpp
+++ source/src/UI/base.cpp
@@ -12,17 +12,18 @@ END_EVENT_TABLE()
 MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size) 
 :wxFrame((wxFrame*) NULL, -1, title, pos, size)
 {
-  //init buttons and other widgets here
+  //Init widgets for menu bar
   mainMenu = new wxMenuBar();
-  auto profileMenu = new wxMenu(); //TODO: define these in the header file
-  auto recordMenu = new wxMenu();
-  auto aboutMenu = new wxMenu();
+  profileMenu = new wxMenu(); 
+  recordMenu = new wxMenu();
+  aboutMenu = new wxMenu();
 
   //add menu options to the "Profile" menu
   profileMenu->Append(MENU_SAVE, wxT("Save profile\tCtrl-S"));
   profileMenu->Append(MENU_LOAD, wxT("Load profile\tCtrl-O"));
   profileMenu->Append(MENU_QUIT, wxT("Quit\tCtrl-Q"));
 
+  //add menu options to "About" menu
   aboutMenu->Append(MENU_ABOUT, wxT("Information"));
 
   //add "Profile" to the bar and activate the bar
@@ -30,9 +31,39 @@ MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& si
   mainMenu->Append(recordMenu, wxT("Recording"));
   mainMenu->Append(aboutMenu, wxT("About"));
   SetMenuBar(mainMenu);
+
+  //Init widgets for button grid
+  windowSizer = new wxBoxSizer(wxVERTICAL);
+  panel = new wxPanel(this, wxID_ANY);
+  buttonGridSizer = new wxFlexGridSizer(4, 4, margin, margin);
+
+  //Create button grid
+  for(int i = 0; i < 4; ++i)
+  {
+    buttonGridSizer->AddGrowableRow(i);
+    buttonGridSizer->AddGrowableCol(i);
+
+    for(int j = 0; j < 4; ++j)
+    {
+      auto button = new wxButton(panel, BUTTON_PLAY, wxT("No sound"), wxDefaultPosition, wxDefaultSize, 0);
+      buttonGridSizer->Add(button);
+    }
+  }
+
+  buttonGridSizer->SetMinSize(wxDefaultSize.GetWidth(), wxDefaultSize.GetHeight());
+  panel->SetSizer(buttonGridSizer);
+
+  windowSizer->Add(panel, 1, wxEXPAND | wxALL, margin);
+  this->SetSizerAndFit(windowSizer);
 }
 
 //Events
+
+void MainFrame::pressPlayButton(wxCommandEvent& event)
+{
+
+}
+
 void MainFrame::saveProfile(wxCommandEvent& event)
 {
  //TODO: implement
